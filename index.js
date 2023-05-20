071f739d2c55f307c8ba50786f6dc6b148f3e7b4
Rakib500538 <rakib2020.tkg@gmail.com>
2023-05-20 18:29:11 +0600

    vercel json file add

--- index.js
+++ index.js
@@ -32,14 +32,22 @@ async function run() {
     await AllToyCollection.createIndex(indexKeys,indexOptions)
     //get allToy in server
     app.get("/allToy", async (req, res) => {
-      const result = await AllToyCollection.find().sort({price:1}).toArray();
+      const result = await AllToyCollection.find().sort({date:1}).toArray();
       res.send(result);
     });
+    // get data limit
+    app.get("/allToy/:limit", async (req, res) => {
+      const limit = parseInt(req.params.limit);
+      console.log(typeof limit);
+      const result = await AllToyCollection.find().sort({date:1}).limit(limit).toArray();
+      res.send(result);
+
+    })
     //get data categories in server
     app.get("/getCategories/:category", async (req, res) =>{
       const subCategory = req.params.category.toLowerCase();
       if(subCategory =="lego-city" || subCategory=="lego-car" || subCategory == "lego-architecture"){
-        const result = await AllToyCollection.find({category:subCategory}).sort({price:-1}).toArray()
+        const result = await AllToyCollection.find({category:subCategory}).toArray()
         res.send(result)
       }
     })
@@ -64,18 +72,36 @@ async function run() {
     app.get("/myToy/:email", async (req, res) => {
       const email = req.params.email;
       const query = {sellerEmail: email };
-      const result = await AllToyCollection.find(query).sort({date:-1}).toArray();
+      const result = await AllToyCollection.find(query).sort({price:-1}).toArray();
       res.send(result);
     });
     //added toy in server
-    app.post("/allToy", async (req, res) => {
+    app.post("/addedToy", async (req, res) => {
       const toy = req.body;
       toy.date = new Date()
       const result = await AllToyCollection.insertOne(toy);
       res.send(result);
     });
+    // updated toy in server
+    app.put("/updateToy/:id", async (req, res)=>{
+      const id = req.params.id;
+      const updateInfo = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert: true};
+      const updateDoc = {
+        $set:{
+          description:updateInfo.description,
+          quantity:updateInfo.quantity,
+          price:updateInfo.price,
+          ...updateInfo
+        }
+      }
+
+      const result = await AllToyCollection.updateOne(filter,updateDoc,options);
+      res.send(result)
+    })
     // removed toy in server
-    app.delete("/singleToy/:id", async (req, res) => {
+    app.delete("/deleteSingelToy/:id", async (req, res) => {
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
         const result = await AllToyCollection.deleteOne(query)
